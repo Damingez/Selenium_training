c27b6835cbdc4832041d33a5a6bd6cadcd6d49f0
Damingez <kowalix93@wp.pl>
2019-11-16 10:30:26 +0100

    NoClassDefFoundError for Firefox

--- java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -4,16 +4,35 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class LoginTest {
 
-    public WebDriver driver = new ChromeDriver();
+
+    /*
+    @Test
+    public void chromeTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("http://localhost/litecart/en/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.name("email")).sendKeys("damian.kowalik94@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("damian");
+        driver.findElement(By.name("login")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.quit();
+    }           */
+
 
     @Test
-    public void myFirstTest() {
+    public void firefoxTest() {
+        WebDriver driver = new FirefoxDriver();
+
         driver.navigate().to("http://localhost/litecart/en/");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.name("email")).sendKeys("damian.kowalik94@gmail.com");
@@ -22,6 +41,25 @@ public class LoginTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.quit();
     }
+
+
+        /*
+    @Test
+    public void ieTest() {
+        WebDriver driver = new InternetExplorerDriver();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+
+        driver.navigate().to("http://localhost/litecart/en/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.name("email")).sendKeys("damian.kowalik94@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("damian");
+        driver.findElement(By.name("login")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.quit();
+    }       */
+
+
 
 
 }
