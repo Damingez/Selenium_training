c978be442cb91879959c6d62241bf4d9b83088d6
Damingez <kowalix93@wp.pl>
2020-01-18 13:00:54 +0100

    Task 14 - checking if links are opening correctly (initial)

---
+++ java-example/src/test/java/ru/stqa/training/selenium/LinksTest.java
@@ -0,0 +1,54 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
+
+public class LinksTest extends TestBase{
+
+
+  @Test
+  public void linksTest()
+  {
+    driver.navigate().to("http://localhost/litecart/admin/login.php");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+
+    driver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
+
+    driver.findElement(By.cssSelector("tr.row td:nth-child(5) a")).click();
+
+   List<WebElement> links = driver.findElements(By.cssSelector("i.fa.fa-external-link"));
+
+    Set<String> oldWindows = driver.getWindowHandles();
+    String originalWindow = driver.getWindowHandle();
+   links.get(0).click();
+    Set<String> currentWindows = driver.getWindowHandles();
+
+    String newWindow = thereIsWindowOtherThan(oldWindows, currentWindows);
+    originalWindow = driver.getWindowHandle();
+    driver.switchTo().window(newWindow);
+
+    System.out.println(driver.getWindowHandle());
+   links.get(1).click();
+    wait.until(numberOfWindowsToBe( 3));
+   // driver.switchTo().window(newWindow);
+     oldWindows = driver.getWindowHandles();
+    System.out.println(oldWindows);
+
+  }
+
+  private String thereIsWindowOtherThan(Set<String> oldWindows,Set<String> currentWindows) {
+
+  currentWindows.removeAll(oldWindows);
+    String newPageId = String.valueOf(currentWindows);
+    return newPageId;
+  }
+
+}
