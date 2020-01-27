9ecb1fb23fd7446ebc96e473d3fe72fd58645467
Damingez <kowalix93@wp.pl>
2020-01-27 18:28:19 +0100

    Task 17 Getting logs for Litecart app

--- java-example/src/test/java/ru/stqa/training/selenium/CheckLogs.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CheckLogs.java
@@ -11,13 +11,9 @@ public class CheckLogs extends TestBase {
   public void checkLogsOccurence()
   {
 
-    driver.navigate().to("http://localhost/litecart/admin/login.php");
-    driver.findElement(By.name("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).sendKeys("admin");
-    driver.findElement(By.name("login")).click();
-    driver.navigate().to("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+    driver.navigate().to("https://www.wp.pl/");
 
-    for (LogEntry l : driver.manage().logs().get("browser").getAll()) {
+    for (LogEntry l : driver.manage().logs().get("driver").getAll()) {
       System.out.println(l);
     }
   }
