976b7668a28557ce7ea70e29eff0f719a3215cac
Damingez <kowalix93@wp.pl>
2020-02-19 12:30:20 +0100

    Task 15 Running remote tests by using selenium grid in local network

--- java-example/src/test/java/ru/stqa/training/selenium/TestRemote.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestRemote.java
@@ -12,9 +12,19 @@ public class TestRemote {
 
   @Test
   public void testIE() throws MalformedURLException {
-    WebDriver driver = new RemoteWebDriver(new URL("http://10.0.2.15:4444/wd/hub"),
+    WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.106:4444/wd/hub"),
             DesiredCapabilities.internetExplorer());
     driver.navigate().to("https://www.google.pl/");
+ //   driver.quit();
+
   }
 
 }
+/*
+ hub
+ node on virtual machine http://192.168.68.10:4444/wd/hub
+java -jar selenium-server-standalone-3.141.59.jar -role hub
+java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.1.106:4444/wd/hub -capabilities browserName=firefox,maxInstances=4 -capabilities browserName=chrome,maxInstances=4
+(on virtual machine) java -jar selenium-server-standalone-3.141.59.jar -role node -host 192.168.68.10 -hub http://192.168.1.106:4444/wd/hub
+
+*/
